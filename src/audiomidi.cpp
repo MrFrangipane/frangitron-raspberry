f5eb261cb1f10dc5b456d04e9da003313939770f
MrFrangipane <mister@frangipane.me>
2018-11-10 18:30:17 +0100

    buffer 2048

--- src/audiomidi.cpp
+++ src/audiomidi.cpp
@@ -9,7 +9,7 @@ void AudioMidi::start()
 {
     RtAudio* _audio;
     int deviceIndex = 2;
-    unsigned int bufferSize = 64;
+    unsigned int bufferSize = 2048;
     _track = Track(bufferSize);
 
     // HACKY POTTER ---------------
