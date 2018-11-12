497b6d1ace91d6de639e83f8c65eee9bc1bb0e8b
MrFrangipane <mister@frangipane.me>
2018-11-12 09:29:31 +0100

    remove filter computations

--- src/track.cpp
+++ src/track.cpp
@@ -21,18 +21,20 @@ void Track::process(Sample const * bufferIn, Sample * bufferOut, nFrame time)
     levelMeterIn.bufferBegin();
     levelMeterOut.bufferBegin();
 
+    /*
     filter.process(bufferIn, _bufferA, time);
     filter.process(_bufferA, _bufferB, time);
     filter.process(_bufferB, _bufferA, time);
     filter.process(_bufferA, _bufferB, time);
+    */
 
     _time = time;
     for( int i = 0; i < _bufferSize; i++ ) {
         _left = i * 2;
         _right = _left + 1;
 
-        bufferOut[_left] = _bufferB[_left] * volume;
-        bufferOut[_right] = _bufferB[_right] * volume;
+        bufferOut[_left] = bufferIn[_left] * volume;
+        bufferOut[_right] = bufferIn[_right] * volume;
 
         levelMeterIn.bufferStep(bufferIn[_left], bufferIn[_right]);
         levelMeterOut.bufferStep(bufferOut[_left], bufferOut[_right]);
