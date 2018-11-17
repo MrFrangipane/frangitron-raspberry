f26690e52541b50d5b341326782dba650d8a0170
MrFrangipane <mister@frangipane.me>
2018-11-17 12:35:17 +0100

    step restore Track::process() : comp only

--- src/track.cpp
+++ src/track.cpp
@@ -21,7 +21,7 @@ void Track::process(Sample const * bufferIn, Sample * bufferOut, const nFrame ti
 {
     levelMeterIn.bufferBegin();
     levelMeterOut.bufferBegin();
-    filter.process(bufferIn, _bufferFilter, time);
+    //filter.process(bufferIn, _bufferFilter, time);
     compressor.process(_bufferFilter, _bufferComp, time);
 
     _time = time;
