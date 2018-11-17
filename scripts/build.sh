887f25cfc4c47608a23d39763f104baf5722d40e
MrFrangipane <mister@frangipane.me>
2018-11-17 12:05:55 +0100

    add post-build run

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,13 @@
+echo "-- pull --"
 cd ~/frangitron-raspberry
 git pull
 chmod +x ~/frangitron-raspberry/scripts/build.sh
 
+echo "-- make --"
 cd ~/frangitron
 qmake ~/frangitron-raspberry/src/frangitron.pro
 make clean
 make -j4
+
+echo "-- run  --"
+./frangitron --platform linuxfb
