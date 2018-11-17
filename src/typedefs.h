7794aaa097440695f8181602470d272555c1d920
MrFrangipane <mister@frangipane.me>
2018-11-17 12:57:11 +0100

    nFrame as int

--- src/typedefs.h
+++ src/typedefs.h
@@ -1,12 +1,10 @@
 #ifndef TYPEDEFS_H
 #define TYPEDEFS_H
 
-#include <cstdint>
-
 #define RTAUDIO_DEBUG
 #define SAMPLE_RATE 48000
 
-typedef uint_fast32_t nFrame;
+typedef int nFrame;
 typedef float Sample; // PiSound supports only up to 32 bits
 
 #endif // TYPEDEFS_H
