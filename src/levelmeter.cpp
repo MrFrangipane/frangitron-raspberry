675f566962f2c5a3d50d581da3c80ec2a3eb3c8c
MrFrangipane <mister@frangipane.me>
2018-11-17 12:04:07 +0100

    remove track processing

--- src/levelmeter.cpp
+++ src/levelmeter.cpp
@@ -2,25 +2,20 @@
 
 void LevelMeter::bufferBegin()
 {
-    /*
     _instantL = 0.0;
     _instantR = 0.0;
     _nStep = 0;
-    */
 }
 
 void LevelMeter::bufferStep(Sample left, Sample right)
 {
-    /*
     _instantL += left * left;
     _instantR += right * right;
     _nStep++;
-    */
 }
 
 void LevelMeter::bufferEnd()
 {
-    /*
     rmsInstantL = 10 * log(2.0 * _instantL / (float)_nStep);
     rmsInstantR = 10 * log(2.0 * _instantR / (float)_nStep);
 
@@ -34,5 +29,4 @@ void LevelMeter::bufferEnd()
     }
 
     _nAverage++;
-    */
 }
